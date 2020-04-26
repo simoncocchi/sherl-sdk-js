88ed95f5dcc38077d5d1d5f735f8e02acf7dc889
Exiam <exiam@pm.me>
2020-04-26 18:54:20 +0200

    build: Update release config file

--- release.config.js
+++ release.config.js
@@ -1,9 +1,14 @@
 module.exports = {
   plugins: [
-    '@semantic-release/commit-analyzer',
-    {
-      preset: 'angular',
-      releaseRules: [{ type: 'docs', scope: 'README', release: 'patch' }],
-    },
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        releaseRules: [{ type: 'docs', scope: 'README', release: 'patch' }],
+      },
+    ],
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    '@semantic-release/github',
   ],
 };
