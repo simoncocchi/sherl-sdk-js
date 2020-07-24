5dc62871fab02aadf53c655ed58cc70e9fe1656c
Simon <s.cocchi@winzana.com>
2020-07-24 11:53:50 +0200

    feat(me): Implement me and update index

--- src/me/actions/get-categories.action.ts
+++
@@ -1,9 +0,0 @@
-// import { IMeResponse } from '../types';
-// import { ProductApi } from '../api/client';
-
-// export const getCategories = async (
-//   organizationId: string,
-// ): Promise<IMeResponse[]> => {
-//   const response = await ProductApi.getCategories(organizationId);
-//   return response.data;
-// };
--- src/me/provider.ts
+++ src/me/provider.ts
@@ -1,25 +1,14 @@
 import { getMe } from './actions';
 
 class MeProvider {
-  // public list(
-  //   page = 1,
-  //   itemsPerPage = 10,
-  //   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  //   filters: { [key: string]: any },
-  // ) {
-  //   return getProducts(page, itemsPerPage, filters);
-  // }
-
-  // public one(id: string) {
-  //   return getProduct(id);
-  // }
 
   /**
    * Get Me.
    *
-   * 
+   * @returns
+   * @memberof MeProvider
    */
-  public me() {
+  public list() {
     return getMe();
   }
 }
