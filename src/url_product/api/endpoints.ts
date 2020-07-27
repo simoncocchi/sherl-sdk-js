514dcd4039fa8a5f3351726d151c0a45c6c843d1
Simon <s.cocchi@winzana.com>
2020-07-27 11:25:28 +0200

    feat(url_product): add endpoint

--- src/url_product/api/endpoints.ts
+++ src/url_product/api/endpoints.ts
@@ -1,6 +1,8 @@
 export const endpoints = {
-  GET_PRODUCT: '/api/shop/products/:id',
-  GET_PRODUCTS: '/api/shop/products',
-  CATEGORIES_ALL: '/api/shop/products/categories/all',
-  GET_CATEGORY: '/api/shop/products/categories/:id',
+  GET_URL_PRODUCT: '/api/public/shop/products/:id',
+  GET_URL_PRODUCTS: '/api/public/shop/products',
+  GET_URL_PRODUCT_SLUG: '/api/public/shop/products/find-one-by-slug/:slug',
+  GET_URL_CATEGORIES: '/api/public/shop/products/categories?organizationSlug=:organizationSlug', //???????
+  GET_URL_CATEGORIES_SLUG: '/api/public/shop/products/categories/find-one-by-slug?slug=:slug', ///???????
+  GET_URL_CATEGORIES_AND_SUB: '/api/public/shop/products/categories-and-subcategories',
 };
